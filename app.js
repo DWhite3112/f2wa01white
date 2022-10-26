495de1a13498d4d81d71530281e381020941d4e0
DeAndre' White <111456327+DWhite3112@users.noreply.github.com>
2022-10-26 15:15:43 -0500

    new endpoint

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use('/mydata', mydataRouter);
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
